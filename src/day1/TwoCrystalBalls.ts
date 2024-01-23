902fd6fe83640e34d1923c105b5eaba3da52ae8c
Daniel Freire <danjfreire@gmail.com>
2024-01-23 07:47:39 -0300

    feat: add TwoCrystalBalls solution

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,23 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    const jumpAmount = Math.floor(Math.sqrt(breaks.length));
 
+    let floor = jumpAmount;
+    while (floor < breaks.length) {
+        if (breaks[floor]) {
+            break;
+        }
+        floor += jumpAmount;
+    }
+
+    const breaksAt = floor;
+    floor -= jumpAmount;
+
+    while (floor < breaksAt) {
+        if (breaks[floor]) {
+            return floor;
+        }
+        floor++;
+    }
+
+    return -1;
 }
