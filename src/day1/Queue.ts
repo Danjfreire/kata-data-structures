0beac1b47d1d958b4939f710c9e2b40b9d0b909a
Daniel Freire <danjfreire@gmail.com>
2024-01-24 06:34:03 -0300

    feat: add queue implementation

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    val: T;
+    next?: Node<T>;
+};
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.length = 0;
+        this.head = undefined;
+        this.tail = undefined;
     }
 
     enqueue(item: T): void {
+        this.length++;
+        const node = { val: item } as Node<T>;
+        if (!this.tail) {
+            this.head = node;
+            this.tail = node;
+            return;
+        }
 
-}
+        this.tail.next = node;
+        this.tail = node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        const headRef = this.head;
+        this.head = this.head.next;
+
+        headRef.next = undefined;
+
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        return headRef.val;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.val;
+    }
 }
