db81e0895ab0a10d41527a96ed6221ae3b7d16e4
Daniel Freire <danjfreire@gmail.com>
2024-01-30 19:28:44 -0300

    feat: add compare binary trees implementation

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,21 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    if (a === null && b === null) {
+        return true;
+    }
 
+    if (a === null || b === null) {
+        return false;
+    }
+
+    if (a.value !== b.value) {
+        return false;
+    }
+
+    const leftIsEqual = compare(a.left, b.left);
+    const rightIsEqual = compare(a.right, b.right);
+
+    return leftIsEqual && rightIsEqual;
 }
