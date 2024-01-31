81f383859ea43a2410db75d32e0ce24dfdba298f
Daniel Freire <danjfreire@gmail.com>
2024-01-31 18:40:16 -0300

    feat: add binary search tree search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,21 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(node: BinaryNode<number> | null, value: number): boolean {
+    // base cases
+    if (node === null) {
+        return false;
+    }
 
+    if (node.value === value) {
+        return true;
+    }
+
+    // recursion
+    if (node.value < value) {
+        return search(node.right, value);
+    }
+
+    return search(node.left, value);
+}
+
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+    return search(head, needle);
 }
