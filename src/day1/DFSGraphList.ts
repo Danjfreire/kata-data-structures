6f830cd432c8ba9ac0c708887fc62934f83ccfca
Daniel Freire <danjfreire@gmail.com>
2024-02-01 17:11:48 -0300

    feat: add dfs on graph

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,51 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(
+    graph: WeightedAdjacencyList,
+    curr: number,
+    needle: number,
+    seen: boolean[],
+    path: number[],
+): boolean {
+    if (curr === needle) {
+        path.push(curr);
+        return true;
+    }
 
+    if (seen[curr]) {
+        return false;
+    }
+
+    // pre
+    seen[curr] = true;
+    path.push(curr);
+
+    // recursion
+    const children = graph[curr];
+    for (let i = 0; i < children.length; i++) {
+        let edge = children[i];
+        if (walk(graph, edge.to, needle, seen, path)) {
+            return true;
+        }
+    }
+
+    // post
+    path.pop();
+
+    return false;
+}
+
+export default function dfs(
+    graph: WeightedAdjacencyList,
+    source: number,
+    needle: number,
+): number[] | null {
+    const path: number[] = [];
+    const seen = new Array(graph.length).fill(false);
+
+    walk(graph, source, needle, seen, path);
+
+    if (path.length === 0) {
+        return null;
+    }
+
+    return path;
 }
